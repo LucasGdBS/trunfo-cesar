bb37766e7b9f269161b6efc77d5b6e1b02d1ec76
Lucas-PC <lgbs@cesar.school>
2024-07-22 18:35:13 -0300

    adicionando libs para interação com movimentos

--- src/Components/Carta/index.jsx
+++ src/Components/Carta/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Carta = ({ destinatario, texto }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-full min-h-screen bg-gradient-to-r from-green-400 to-green-600 rounded-3xl p-4 sm:min-h-[calc(100vh-7rem)]">
+    <div className="flex flex-col items-center justify-center h-full min-h-screen bg-gradient-to-r from-green-400 to-green-600 rounded-3xl p-4 sm:min-h-[calc(100vh-7rem)] select-none">
       <div className="text-white text-2xl text-center font-bold mb-9">Esta mensagem é para: {destinatario}</div>
       <div className="text-white text-2xl text-center">{texto}</div>
     </div>
