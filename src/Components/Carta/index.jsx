e3715efaf1395007131e3a80afe21a3572dccf87
Lucas-PC <lgbs@cesar.school>
2024-07-22 17:20:20 -0300

    criação prototipo de carta

--- src/Components/Carta/index.jsx
+++ src/Components/Carta/index.jsx
@@ -1,18 +1,16 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Carta = ({ texto }) => {
+const Carta = ({ destinatario, texto }) => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex items-center justify-center h-full max-w-sm w-full bg-gradient-to-r from-green-400 to-green-600 rounded-3xl p-4">
-        <div className="text-white text-2xl">
-          {texto}
-        </div>
-      </div>
+    <div className="flex flex-col items-center justify-center h-full min-h-screen bg-gradient-to-r from-green-400 to-green-600 rounded-3xl p-4 sm:min-h-[calc(100vh-7rem)]">
+      <div className="text-white text-2xl text-center font-bold mb-9">Esta mensagem é para: {destinatario}</div>
+      <div className="text-white text-2xl text-center">{texto}</div>
     </div>
   );
 };
 
 Carta.propTypes = {
+  destinatario: PropTypes.string.isRequired,
   texto: PropTypes.string.isRequired,
 };
 
