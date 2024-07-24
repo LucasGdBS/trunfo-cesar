e17b17f588a2fd8276a210092f11879354e01aa4
Lucas-PC <lgbs@cesar.school>
2024-07-24 04:18:21 -0300

    adicionando SwipeHint ao baralho

--- src/Components/Baralho/index.jsx
+++ src/Components/Baralho/index.jsx
@@ -1,26 +1,47 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import Carta from "../Carta";
+import SwipeHint from "../SwipeHint";
 import PropTypes from "prop-types";
 import "swiper/css";
 
 import "swiper/css/effect-cards";
 
 import { EffectCards, Keyboard, A11y } from "swiper/modules";
+import { useEffect, useState } from "react";
 
 function Baralho({ cartas }) {
+  const [visible, setVisible] = useState(true);
+  const [touchEnabled, setTouchEnabled] = useState(false);
+
+  // Timer para desaparecer a SwipeHint
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+      setTouchEnabled(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Swiper
+      key={touchEnabled ? 'enabled' : 'disabled'}
+      allowTouchMove={touchEnabled}
       rewind={true}
       effect="cards"
       grabCursor={true}
       modules={[EffectCards, Keyboard, A11y]}
       spaceBetween={50}
       slidesPerView={1}
-      a11y={{enabled: true}}
+      a11y={{ enabled: true }}
       keyboard={{
         enabled: true,
       }}
     >
+      {visible && (
+        <SwiperSlide>
+          <SwipeHint />
+        </SwiperSlide>
+      )}
       {cartas.map((carta, index) => (
         <SwiperSlide key={index}>
           <Carta destinatario={carta.destinatario} texto={carta.texto} />
