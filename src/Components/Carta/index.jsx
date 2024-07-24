853943f9f5aa197103d8ffc635d600da8b22a9e6
Lucas-PC <lgbs@cesar.school>
2024-07-24 04:18:02 -0300

    Criação do componente SwipeHint

--- src/Components/Carta/index.jsx
+++ src/Components/Carta/index.jsx
@@ -1,12 +1,14 @@
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
-const Carta = ({ destinatario, texto}) => {
-
+const Carta = ({ destinatario, texto, className }) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full min-h-screen bg-gradient-to-br from-slate-600 to-slate-950 rounded-3xl p-4 sm:min-h-[calc(100vh-7rem)] select-none">
-      <div className="text-white text-2xl text-center font-bold mb-9">
-        Esta mensagem é para: {destinatario}
-      </div>
+    <div className={classNames("flex flex-col items-center justify-center w-full h-full min-h-screen bg-gradient-to-br from-slate-600 to-slate-950 rounded-3xl p-4 sm:min-h-[calc(100vh-7rem)] select-none", className)}>
+      {destinatario && (
+        <div className="text-white text-2xl text-center font-bold mb-9">
+          Esta mensagem é para: {destinatario}
+        </div>
+      )}
       <div className="text-white text-2xl text-center">{texto}</div>
       <div className="absolute top-0 left-0 w-16 h-16 border-b-8 border-r-8 border-b-orange-500 border-r-orange-500 rounded-full"></div>
     </div>
@@ -16,8 +18,7 @@ const Carta = ({ destinatario, texto}) => {
 Carta.propTypes = {
   destinatario: PropTypes.string.isRequired,
   texto: PropTypes.string.isRequired,
-  onSwipeLeft: PropTypes.func,
-  onSwipeRight: PropTypes.func,
+  className: PropTypes.string,
 };
 
 export default Carta;
---
+++ src/Components/SwipeHint/index.jsx
@@ -0,0 +1,14 @@
+import Carta from "../Carta";
+
+function SwipeHint() {
+  return (
+    <>
+      <Carta 
+				texto={"Para começar a jogar deslize para os lados!"}
+				className={"opacity-80"}	
+			/>
+    </>
+  );
+}
+
+export default SwipeHint;
