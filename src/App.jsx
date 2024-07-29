7f9b5c5602142536a0aad84f2930c774fdc53a1e
Lucas-PC <lgbs@cesar.school>
2024-07-29 15:24:33 -0300

    implementando função para randomizar cartas

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import cards from "../cards.json";
 import Informacao from "./Components/Informacao";
 
 function App() {
-  const cartas = cards
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Troca os elementos
+    }
+    return array;
+  }
+
+  const cartas = shuffleArray(cards)
 
   return (
     <>
