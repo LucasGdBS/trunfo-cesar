1d4494d5dc409c00bc274859104d1641bce1cd44
Lucas-PC <lgbs@cesar.school>
2024-07-29 15:11:58 -0300

    Ajustando a Carta ao novo cabeçalho

--- src/Components/Carta/index.jsx
+++ src/Components/Carta/index.jsx
@@ -1,16 +1,23 @@
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import CardHeader from "../CardHeader";
 
 const Carta = ({ destinatario, texto, className }) => {
   return (
-    <div className={classNames("flex flex-col items-center justify-center w-full h-full min-h-screen bg-gradient-to-br from-slate-600 to-slate-950 rounded-3xl p-4 sm:min-h-[calc(100vh-7rem)] select-none", className)}>
+    <div
+      className={classNames(
+        "flex flex-col items-center w-full h-full min-h-screen bg-gradient-to-br from-slate-600 to-slate-950 rounded-3xl p-4 py-16 sm:min-h-[calc(100vh-7rem)] select-none",
+        className
+      )}
+    >
       {destinatario && (
-        <div className="text-white text-2xl text-center font-bold mb-9">
-          Esta mensagem é para: {destinatario}
+        <div className="w-full flex justify-start">
+          <CardHeader remetente={destinatario}/>
         </div>
       )}
-      <div className="text-white text-2xl text-center">{texto}</div>
-      <div className="absolute top-0 left-0 w-16 h-16 border-b-8 border-r-8 border-b-orange-500 border-r-orange-500 rounded-full"></div>
+      <div className="flex-grow flex items-center justify-center w-full">
+        <div className="text-white text-2xl text-center ">{texto}</div>
+      </div>
     </div>
   );
 };
